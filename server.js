6c0ccbf42b35f810d72717f132e685242d619e8f
TienNguyen <ngoctiennguyen155@gmail.com>
2020-05-09 22:44:22 +0700

    sua giao dien done

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const mongodb = require('mongoose');
 const cors = require('cors');
 require('dotenv/config');
 
+// connect mongodb
+mongodb
+  .connect(process.env.DB_CONNECT, {
+    dbName: 'tmdt',
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connect database seccess !!!');
+  });
 
-// connect mongodb 
-mongodb.connect(process.env.DB_CONNECT, {
-  dbName: 'tmdt',
-  useNewUrlParser: true,
-  useUnifiedTopology : true
-}).then(() => {
-  console.log('Connect database seccess !!!');
-});
+app.use(express.static('./public'));
 
-app.use(express.static("./public"));
-
-app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set('views', './views');
+app.set('view engine', 'ejs');
 const port = 3000;
-app.listen(port,console.log(`Listening on port ${port}...`));
+app.listen(port, console.log(`Listening on port ${port}...`));
 
 const schema = require('./model/schema');
 // app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 // this middleware use to build restful api so need this line to fix 'no access control allow origin' OK
 app.use(cors());
 //
 
 app.post('/', async function (req, res) {
   const Schema = new schema({
-    title: req.body.title
+    title: req.body.title,
   });
 
   try {
@@ -42,61 +43,55 @@ app.post('/', async function (req, res) {
   }
 });
 
-
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.get("/blog-details", function (req, res) {
-  res.render("blog-details");
+app.get('/blog-details', function (req, res) {
+  res.render('blog-details');
 });
 
-app.get("/shop-grid", function (req, res) {
-  res.render("shop-grid");
+app.get('/shop-grid', function (req, res) {
+  res.render('shop-grid');
 });
 
-app.get("/shoping-cart", function (req, res) {
-  res.render("shoping-cart");
+app.get('/shoping-cart', function (req, res) {
+  res.render('shoping-cart');
 });
 
-app.get("/shop-details", function (req, res) {
-  res.render("shop-details");
+app.get('/shop-details', function (req, res) {
+  res.render('shop-details');
 });
 
-app.get("/blog", function (req, res) {
-  res.render("blog");
+app.get('/blog', function (req, res) {
+  res.render('blog');
 });
 
-app.get("/checkout", function (req, res) {
-  res.render("checkout");
+app.get('/checkout', function (req, res) {
+  res.render('checkout');
 });
 
-app.get("/contact", function (req, res) {
-  res.render("contact");
+app.get('/contact', function (req, res) {
+  res.render('contact');
 });
 
-app.get("/index", function (req, res) {
-  res.render("index");
+app.get('/index', function (req, res) {
+  res.render('index');
 });
 
-app.get("/main", function (req, res) {
-  
-});
+app.get('/main', function (req, res) {});
 
-
-
-// route contact 
+// route contact
 const contactmessage = require('./model/contact');
 app.post('/contact', function (req, res) {
   const newcontact = new contactmessage({
-    name : req.body.name,
-    email : req.body.email,
-    message : req.body.message
+    name: req.body.name,
+    email: req.body.email,
+    message: req.body.message,
   });
-  newcontact.save(); 
-  
-  res.render('contact');
-})
+  newcontact.save();
 
+  res.render('contact');
+});
 
 //

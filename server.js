b4c4f1668bf2e9a7c4f2d15868e65e857b249cf2
TienNguyen <ngoctiennguyen155@gmail.com>
2020-05-13 21:38:42 +0700

    contact final done, checkmail all final

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.listen(port, console.log(`Listening on port ${port}...`));
 const schema = require('./model/schema');
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // this middleware use to build restful api so need this line to fix 'no access control allow origin' OK
 app.use(cors());
 //
@@ -49,51 +50,51 @@ app.use(cors());
 //     res.send(err);
 //   }
 // });
-// load product
+// load product route index 
+
 const dssanpham = require('./model/saleproduct');
 const dsspnoibat = require('./model/sanphamnoibat');
 app.get('/', async (req, res) => {
-  const data = await dssanpham.find({trangthai:"con"});
+  const data = await dssanpham.find({trangthai:"con",hieuluc:"con"});
   const data2 = await dsspnoibat.find({ trangthai: "con" });
-  res.render('index', {listsp:data,listspnoibat:data2});
+  res.render('index', {listsp:data,listspnoibat:data2,message:""});
+});
+app.get('/index', async (req, res) => {
+  const data = await dssanpham.find({ trangthai: 'con', hieuluc: 'con' });
+  const data2 = await dsspnoibat.find({ trangthai: 'con' });
+  res.render('index', { listsp: data, listspnoibat: data2,message:"" });
 });
 
+// route blog detail
 app.get('/blog-details', function (req, res) {
   res.render('blog-details');
 });
-
+// route shop grid
 app.get('/shop-grid', function (req, res) {
   res.render('shop-grid');
 });
+//route admin
 app.get('/admin', function (req, res) {
   res.render('admin');
 });
-
+//route shopping cart
 app.get('/shoping-cart', function (req, res) {
   res.render('shoping-cart');
 });
-
+//route shop details
 app.get('/shop-details', function (req, res) {
   res.render('shop-details');
 });
-
+//route blog
 app.get('/blog', function (req, res) {
   res.render('blog');
 });
-
+//route check out
 app.get('/checkout', function (req, res) {
   res.render('checkout');
 });
 
-app.get('/contact', function (req, res) {
-  res.render('contact', { page: '2' });
-});
 
-app.get('/index', async (req, res) => {
-  const data = await dssanpham.find({ trangthai: 'con' });
-  const data2 = await dsspnoibat.find({ trangthai: 'con' });
-  res.render('index', { listsp: data, listspnoibat: data2 });
-});
 
 app.get('/main', function (req, res) {});
 app.get('/about', function (req, res) {
@@ -101,8 +102,11 @@ app.get('/about', function (req, res) {
 });
 
 // route contact
+app.get('/contact', function (req, res) {
+  res.render('contact');
+});
 const contactmessage = require('./model/contact');
-app.post('/contact', function (req, res) {
+app.post('/contact', async function (req, res) {
   const newcontact = new contactmessage({
     name: req.body.name,
     email: req.body.email,
@@ -110,7 +114,9 @@ app.post('/contact', function (req, res) {
   });
   newcontact.save();
 
-  res.render('index');
+  const data = await dssanpham.find({ trangthai: 'con', hieuluc: 'con' });
+  const data2 = await dsspnoibat.find({ trangthai: 'con' });
+  res.render('index', { listsp: data, listspnoibat: data2,message:"Thanks for your contact !!!" });
 });
 
 //
